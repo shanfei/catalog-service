02aad91d4dfe8b33b54345d3697f02fa5d1cfeaf
Fei Shan <shanfeishanfei@gmail.com>
2022-11-21 14:15:45 -0800

    add changes for flywight database migration

--- src/main/java/com/polarbookshop/catalogservice/domain/Book.java
+++ src/main/java/com/polarbookshop/catalogservice/domain/Book.java
@@ -37,6 +37,8 @@ public record Book(
         )
         Double price,
 
+        String publisher,
+
         @CreatedDate
         Instant createdDate,
 
@@ -48,10 +50,10 @@ public record Book(
 
 ) {
         public static Book of(
-                String isbn, String title, String author, Double price
+                String isbn, String title, String author, Double price, String publisher
         ) {
                 return new Book(
-                        null, isbn, title, author, price, null, null,0
+                        null, isbn, title, author, price, publisher, null, null,0
                 );
         }
 }
--- src/test/java/com/polarbookshop/catalogservice/web/BookJsonTests.java
+++ src/test/java/com/polarbookshop/catalogservice/web/BookJsonTests.java
@@ -16,7 +16,7 @@ public class BookJsonTests {
 
     @Test
     void testSerialize() throws Exception {
-        var book = Book.of("1234567890", "Title", "Author", 9.90);
+        var book = Book.of("1234567890", "Title", "Author", 9.90, "Polarsophia");
         var jsonContent = json.write(book);
         assertThat(jsonContent).extractingJsonPathStringValue("@.isbn")
                 .isEqualTo(book.isbn());
@@ -34,11 +34,12 @@ public class BookJsonTests {
                         "isbn": "1234567890",
                         "title": "Title",
                         "author": "Author",
-                        "price": 9.90
+                        "price": 9.90,
+                        "publisher" : "Polarsophia"
                         }
                         """;
         assertThat(json.parse(content))
                 .usingRecursiveComparison()
-                .isEqualTo(Book.of("1234567890", "Title", "Author", 9.90));
+                .isEqualTo(Book.of("1234567890", "Title", "Author", 9.90, "Polarsophia"));
     }
 }
